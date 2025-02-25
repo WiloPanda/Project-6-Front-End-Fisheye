b7fec97467a2ff5ae3e179316c77f14a8b058c80
WiloPanda <wilona.middendorp@gmail.com>
2025-02-25 10:39:31 +0100

    CSS landing page Done
    Css of the landing page OK

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,68 @@
 //Mettre le code JavaScript lié à la page photographer.html
+document.addEventListener("DOMContentLoaded", function () {
+
+    async function init() {
+        const id = getId();
+        console.log(id);
+        const datas = await getData();
+        console.log(datas);
+        const photographer = sortPhotographer(id, datas.photographers);
+        console.log(photographer);
+        const media = sortMedia(id, datas.media);
+        console.log(media);
+    }
+
+    init();
+
+    /**
+     * Get the ID of the photographer from the URL.
+     * @returns {string} La valeur du paramètre "id".
+     */
+    function getId() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const id = urlParams.get("id");
+        return id;
+    }
+
+    /**
+     * Get data from the JSON file.
+     * @returns {object} Les données des photographes.
+     */
+    function getData() {
+        return fetch("data/photographers.json")
+            .then(function (response) {
+                return response.json();
+            })
+            .catch(error => console.error(error));
+    }
+
+    /**
+     * Sort the photographer by ID.
+     * @param {string} id - The ID of the photographer.
+     * @param {object} photographers - The list of photographers.
+     * @returns {object} The photographer.
+     */
+    function sortPhotographer(id, photographers) {
+        return photographers.find(photographer => photographer.id == id);
+    }
+
+
+    function sortMedia(photographerId, media) {
+        return media.filter(media => media.photographerId == photographerId);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,19 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
-
+    const picture = `assets/photos/Sample Photos/Photographers ID Photos/${portrait}`;
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const article = document.createElement('article');
+        article.insertAdjacentHTML('beforeend', `
+            <a href="./photographer.html?id=${id}" tabindex="0" aria-label="voir la page de ${name}">
+            <img src="${picture}" alt="photo de profile de ${name}"/>   
+            <h2>${name}</h2>
+            </a>
+            <h3>${city}, ${country}</h3>
+            <p class="tagline">${tagline}</p>
+            <p class="price" aria-label="le coût de la prestation de ${name} est de ${price}€ par jour">${price}€/jour</p>
+        `);
+        return article;
     }
     return { name, picture, getUserCardDOM }
 }
