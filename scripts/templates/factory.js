93cf741f997497c1ee4db8ad0b930dec4d8a3fdb
WiloPanda <wilona.middendorp@gmail.com>
2025-03-06 14:45:43 +0100

    Like

--- scripts/templates/factory.js
+++ scripts/templates/factory.js
@@ -9,7 +9,7 @@ class MainMedia {
         * @param {number} data.id - l'ID unique du média.
     */
     constructor(media) {
-        const { id, likes, title } = media;
+        const { date, price, id, likes, title } = media;
         this.title = title;
         this.likes = likes;
         this.id = id;
@@ -32,6 +32,27 @@ class MainMedia {
                 </div>  
             `
         )
+
+        let likeHeart = article.getElementsByClassName('heart')[0]
+        likeHeart.addEventListener('click', function incLike(e) { // Ajout de l'évènement au clique sur un coeur permettant de liker un média.
+
+            likeHeart.classList.add('heartliked');
+            let likeValue = parseInt(e.target.parentElement.textContent)
+            let intValue = e.target.parentElement.querySelector('p')
+            let totalLikes = document.getElementById("totalLikes")
+
+            intValue.innerHTML = likeValue + 1;
+            totalLikes.innerHTML = parseInt(totalLikes.textContent) + 1;
+
+            likeHeart.removeEventListener('click', incLike) // Retrait de l'évènement une fois un like effectué.
+        })
+
+        likeHeart.addEventListener('keydown', (e) => { // Ajout de l'évènement à l'appui sur la touche "entrée" sur un coeur permettant de liker un média.
+            if (e.key === 'Enter') {
+                likeHeart.click();
+            }
+        })
+
         return (article);
     }
 }
